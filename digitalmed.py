96612b33aff7ef3c272ec73a59fd4d80aa55f9c3
upaasanakrishnan <49619402+upaasanakrishnan@users.noreply.github.com>
2019-04-15 00:49:07 +0530

    Update digitalmed.py

--- digitalmed.py
+++ digitalmed.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 29 01:04:23 2018
 
-@author: 1022316
-"""
-
-#Classification of cancer dignosis
+#Classification of breast cancer stage
 #importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,8 +7,6 @@ import pandas as pd
 
 #importing the dataset 
 dataset = pd.read_csv('C:\Machine learning\cancer_data\cancer.csv')
-X = dataset.iloc[:, 1:31].values
-Y = dataset.iloc[:, 31].values
 
 dataset.head()
 
@@ -46,43 +38,8 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#Fitting the Logistic Regression Algorithm to the Training Set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, Y_train)
-#95.8 Acuracy
 
-#Fitting K-NN Algorithm
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
-classifier.fit(X_train, Y_train)
-#95.1 Acuracy
-
-#Fitting SVM
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
-classifier.fit(X_train, Y_train) 
-#97.2 Acuracy
-
-#Fitting K-SVM
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
-classifier.fit(X_train, Y_train)
-#96.5 Acuracy
-
-#Fitting Naive_Bayes
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train, Y_train)
-#91.6 Acuracy
-
-#Fitting Decision Tree Algorithm
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(X_train, Y_train)
-#95.8 Acuracy
-
-#Fitting Random Forest Classification Algorithm
+#Fitting Random Forest Classification Algorithm to the training set
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, Y_train)
@@ -91,10 +48,7 @@ classifier.fit(X_train, Y_train)
 #predicting the Test set results
 Y_pred = classifier.predict(X_test)
 
-#Creating the confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(Y_test, Y_pred)
-c = print(cm[0, 0] + cm[1, 1])
+
 
 
 
